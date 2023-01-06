6bf9133f7e64efd6bd20b0426ab6ac957ae73525
Cyril Trichereau <cyril.trichereau@niji.fr>
2023-01-06 19:27:13 +0100

    update sandbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,17 @@
 import { Heading, Stack, Text } from "@chakra-ui/react";
+import { PanelTabs } from "../src/components/PanelsTabs";
 
 export const Homepage = () => {
   return (
     <>
-      {/* <Heading as="h1">SandBox test for Chakra UI Tree Shaking</Heading>
+      <Heading bg={"pink"} as="h1">
+        SandBox test for Chakra UI Tree Shaking
+      </Heading>
       <Stack spacing={3}>
         <Text fontSize="6xl">(6xl) In love with React & Next</Text>
-        <Text fontSize="5xl">(5xl) In love with React & Next</Text>
-        <Text fontSize="4xl">(4xl) In love with React & Next</Text>
-        <Text fontSize="3xl">(3xl) In love with React & Next</Text>
-        <Text fontSize="2xl">(2xl) In love with React & Next</Text>
-        <Text fontSize="xl">(xl) In love with React & Next</Text>
-        <Text fontSize="lg">(lg) In love with React & Next</Text>
-        <Text fontSize="md">(md) In love with React & Next</Text>
-        <Text fontSize="sm">(sm) In love with React & Next</Text>
-        <Text fontSize="xs">(xs) In love with React & Next</Text>
-      </Stack> */}
+        <Text fontSize="6xl">(6xl) In love with React & Next</Text>
+      </Stack>
+      <PanelTabs />
       <h2>Je suis là</h2>
     </>
   );
