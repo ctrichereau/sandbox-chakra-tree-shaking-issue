4f01aac5bd3c5903443637f63827690aa7f791be
Cyril Trichereau <cyril.trichereau@niji.fr>
2023-01-09 07:16:11 +0100

    update sandbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
-import { Heading, Stack, Text } from "@chakra-ui/react";
-import { PanelTabs } from "../src/components/PanelsTabs";
+import { Heading, Center, Text } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export const Homepage = () => {
   return (
     <>
-      <Heading bg={"pink"} as="h1">
-        SandBox test for Chakra UI Tree Shaking
-      </Heading>
-      <Stack spacing={3}>
-        <Text fontSize="6xl">(6xl) In love with React & Next</Text>
-        <Text fontSize="6xl">(6xl) In love with React & Next</Text>
-      </Stack>
-      <PanelTabs />
+      <ChakraProvider>
+        <Center m="6" w="full" flexDirection="column">
+          <Heading as="h1" color="blue.400">
+            SandBox test for Chakra UI Tree Shaking
+          </Heading>
+          <Text fontSize="xl" color="green.200">
+            In love with React & Next
+          </Text>
+        </Center>
+      </ChakraProvider>
     </>
   );
 };
